c44d8012c33532cedc13cad044c1bdc5da572060
Santhosh K <Santhosh.K@g7cr.com>
2025-05-08 12:45:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) =>
-  res.send("🚀 CI/CD working! Deployed via Azure DevOps.")
-);
+app.get('/', (req, res) => res.send('missing quote);
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
