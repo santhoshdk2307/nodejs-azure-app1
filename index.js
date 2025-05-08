9e54a528be85eb292156dea91ca8d42650766a72
Santhosh K <Santhosh.K@g7cr.com>
2025-05-08 12:50:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('missing quote);
+app.get('/', (req, res) => res.send('missisdsdng quote);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
